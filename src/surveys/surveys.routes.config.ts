89889ec8864af8d58c864197806f646b3aeeb282
Nils Kortmann <kortmannnils@gmail.com>
2024-03-13 23:23:26 +0100

    use custom route for survey finalization

--- src/surveys/surveys.routes.config.ts
+++ src/surveys/surveys.routes.config.ts
@@ -3,7 +3,7 @@ import { CommonRoutesConfig } from '../common/common.routes.config';
 import PermissionMiddleware from '../common/middleware/permission.middleware';
 import SurveysMiddleware from '../surveys/middleware/surveys.middleware';
 import SurveysController from './controllers/surveys.controller';
-import { body, oneOf } from 'express-validator';
+import { body } from 'express-validator';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import PagingMiddleware from '../common/middleware/paging.middleware';
 import AuthMiddleware from '../auth/middleware/auth.middleware';
@@ -59,6 +59,30 @@ export class SurveysRoutes extends CommonRoutesConfig {
         SurveysController.createSurvey,
       );
 
+    this.app.route(`/surveys/:surveyId/finalize`).patch(
+      AuthMiddleware.validAuthorizationNeeded(true, false),
+      SurveysMiddleware.validateSurveyExists,
+      PermissionMiddleware.onlySurveyOwnerOrAdminCanDoThisAction,
+      body('_id').not().exists(),
+      body('name').not().exists(),
+      body('greeting').not().exists(),
+      body('description').not().exists(),
+      body('owner').not().exists(),
+      body('created').not().exists(),
+      body('edited').not().exists(),
+      body('draft')
+        .isBoolean({ strict: true })
+        .custom(value => value === false)
+        .exists(),
+      body('archived').not().exists(),
+      body('questions').not().exists(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
+      SurveysMiddleware.validateSurveyModifiable,
+      SurveysMiddleware.validateSetStartEndDates,
+      SurveysMiddleware.validateSurveyFinalizable,
+      SurveysController.patch,
+    );
+
     this.app.param(`surveyId`, SurveysMiddleware.extractSurveyId);
 
     this.app
@@ -83,53 +107,33 @@ export class SurveysRoutes extends CommonRoutesConfig {
       AuthMiddleware.validAuthorizationNeeded(true, false),
       SurveysMiddleware.validateSurveyExists,
       PermissionMiddleware.onlySurveyOwnerOrAdminCanDoThisAction,
-      oneOf([
-        [
-          body('_id').not().exists(),
-          body('name').not().exists(),
-          body('greeting').not().exists(),
-          body('description').not().exists(),
-          body('owner').not().exists(),
-          body('created').not().exists(),
-          body('edited').not().exists(),
-          body('draft')
-            .isBoolean({ strict: true })
-            .custom(value => value === false)
-            .exists(),
-          body('archived').not().exists(),
-          body('questions').not().exists(),
-        ],
-        [
-          body('_id').not().exists(),
-          body('name')
-            .isString()
-            .isLength({ min: 1, max: 50 })
-            .withMessage('Der Name muss zwischen 1 und 50 Zeichen lang sein.')
-            .optional(),
-          body('description')
-            .isString()
-            .isLength({ min: 1, max: 150 })
-            .withMessage(
-              'Die Beschreibung muss zwischen 1 und 150 Zeichen lang sein.',
-            )
-            .optional(),
-          body('greeting')
-            .isString()
-            .isLength({ min: 1, max: 300 })
-            .withMessage(
-              'Die Begrüßung muss zwischen 1 und 300 Zeichen lang sein.',
-            )
-            .optional(),
-          body('startDate').isISO8601().toDate().optional(),
-          body('endDate').isISO8601().toDate().optional(),
-          body('owner').not().exists(),
-          body('created').not().exists(),
-          body('edited').not().exists(),
-          body('draft').not().exists(),
-          body('archived').isBoolean().optional(),
-          body('questions').not().exists(),
-        ],
-      ]),
+
+      body('_id').not().exists(),
+      body('name')
+        .isString()
+        .isLength({ min: 1, max: 50 })
+        .withMessage('Der Name muss zwischen 1 und 50 Zeichen lang sein.')
+        .optional(),
+      body('description')
+        .isString()
+        .isLength({ min: 1, max: 150 })
+        .withMessage(
+          'Die Beschreibung muss zwischen 1 und 150 Zeichen lang sein.',
+        )
+        .optional(),
+      body('greeting')
+        .isString()
+        .isLength({ min: 1, max: 300 })
+        .withMessage('Die Begrüßung muss zwischen 1 und 300 Zeichen lang sein.')
+        .optional(),
+      body('startDate').isISO8601().toDate().optional(),
+      body('endDate').isISO8601().toDate().optional(),
+      body('owner').not().exists(),
+      body('created').not().exists(),
+      body('edited').not().exists(),
+      body('draft').not().exists(),
+      body('archived').isBoolean().optional(),
+      body('questions').not().exists(),
       BodyValidationMiddleware.verifyBodyFieldsErrors,
       SurveysMiddleware.validateSurveyModifiable,
       SurveysMiddleware.validateSetStartEndDates,
@@ -137,63 +141,46 @@ export class SurveysRoutes extends CommonRoutesConfig {
       SurveysController.put,
     );
 
-    this.app.route(`/surveys/:surveyId`).patch(
-      AuthMiddleware.validAuthorizationNeeded(true, false),
-      SurveysMiddleware.validateSurveyExists,
-      PermissionMiddleware.onlySurveyOwnerOrAdminCanDoThisAction,
-      oneOf([
-        [
-          body('_id').not().exists(),
-          body('name').not().exists(),
-          body('description').not().exists(),
-          body('greeting').not().exists(),
-          body('owner').not().exists(),
-          body('created').not().exists(),
-          body('edited').not().exists(),
-          body('draft')
-            .isBoolean({ strict: true })
-            .custom(value => value === false)
-            .exists(),
-          body('archived').not().exists(),
-          body('questions').not().exists(),
-        ],
-        [
-          body('_id').not().exists(),
-          body('name')
-            .isString()
-            .isLength({ min: 1, max: 50 })
-            .withMessage('Der Name muss zwischen 1 und 50 Zeichen lang sein.')
-            .optional(),
-          body('description')
-            .isString()
-            .isLength({ min: 1, max: 150 })
-            .withMessage(
-              'Die Beschreibung muss zwischen 1 und 150 Zeichen lang sein.',
-            )
-            .optional(),
-          body('greeting')
-            .isString()
-            .isLength({ min: 1, max: 300 })
-            .withMessage(
-              'Die Begrüßung muss zwischen 1 und 300 Zeichen lang sein.',
-            )
-            .optional(),
-          body('startDate').isISO8601().toDate().optional(),
-          body('endDate').isISO8601().toDate().optional(),
-          body('owner').not().exists(),
-          body('created').not().exists(),
-          body('edited').not().exists(),
-          body('draft').not().exists(),
-          body('archived').isBoolean().optional(),
-          body('questions').not().exists(),
-        ],
-      ]),
-      BodyValidationMiddleware.verifyBodyFieldsErrors,
-      SurveysMiddleware.validateSurveyModifiable,
-      SurveysMiddleware.validateSetStartEndDates,
-      SurveysMiddleware.validateSurveyFinalizable,
-      SurveysController.patch,
-    );
+    this.app
+      .route(`/surveys/:surveyId`)
+      .patch(
+        AuthMiddleware.validAuthorizationNeeded(true, false),
+        SurveysMiddleware.validateSurveyExists,
+        PermissionMiddleware.onlySurveyOwnerOrAdminCanDoThisAction,
+        body('_id').not().exists(),
+        body('name')
+          .isString()
+          .isLength({ min: 1, max: 50 })
+          .withMessage('Der Name muss zwischen 1 und 50 Zeichen lang sein.')
+          .optional(),
+        body('description')
+          .isString()
+          .isLength({ min: 1, max: 150 })
+          .withMessage(
+            'Die Beschreibung muss zwischen 1 und 150 Zeichen lang sein.',
+          )
+          .optional(),
+        body('greeting')
+          .isString()
+          .isLength({ min: 1, max: 300 })
+          .withMessage(
+            'Die Begrüßung muss zwischen 1 und 300 Zeichen lang sein.',
+          )
+          .optional(),
+        body('startDate').isISO8601().toDate().optional(),
+        body('endDate').isISO8601().toDate().optional(),
+        body('owner').not().exists(),
+        body('created').not().exists(),
+        body('edited').not().exists(),
+        body('draft').not().exists(),
+        body('archived').isBoolean().optional(),
+        body('questions').not().exists(),
+        BodyValidationMiddleware.verifyBodyFieldsErrors,
+        SurveysMiddleware.validateSurveyModifiable,
+        SurveysMiddleware.validateSetStartEndDates,
+        SurveysMiddleware.validateSurveyFinalizable,
+        SurveysController.patch,
+      );
 
     return this.app;
   }
